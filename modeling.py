f29026b8feaf81e030ba9c57904e7511652b45ad
SaiKeshav <keshavkolluru@gmail.com>
2019-04-24 09:58:15 +0530

    test

--- modeling.py
+++ modeling.py
@@ -707,7 +707,7 @@ def attention_layer(from_tensor,
                                  1.0 / math.sqrt(float(size_per_head)))
 
   # tf.logging.set_verbosity(tf.logging.INFO)
-  tf.print(tf.shape(attention_scores), output_stream=open('file://tmp.txt','a'))
+  tf.print(tf.shape(attention_scores), output_stream=open('file:///content/bert_repo/tmp.txt','a'))
   # tf.contrib.eager.py_func(func=my_print, inp=[attention_scores], Tout=[])
   if(last_layer == True):
     tf.print(tf.shape(attention_scores), output_stream=sys.stdout)
