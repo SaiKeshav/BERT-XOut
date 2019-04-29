0450f99a89a0df7262c608ce134e81d2786cb069
SaiKeshav <keshavkolluru@gmail.com>
2019-04-29 11:47:08 +0530

    modeling:

--- modeling.py
+++ modeling.py
@@ -241,6 +241,7 @@ class BertModel(object):
           embs = [pool(self.sequence_output, 1, att_type)]
           for i in range(heads):
             out = tf.layers.dense(self.sequence_output, middle_dim, kernel_initializer=create_initializer(config.initializer_range), name='mh0_'+str(i))
+            print(out.name)
             out = self.swish(out)
             out = tf.layers.dense(out, final_dim, activation=tf.tanh, kernel_initializer=create_initializer(config.initializer_range), name='mh1_'+str(i))
             embs.append(pool(out, 1, att_type))  
