10a6786818c878832b3ddceb5ebbd2fcabb04321
SaiKeshav <keshavkolluru@gmail.com>
2019-04-29 12:19:03 +0530

    run_classifier:

--- run_classifier.py
+++ run_classifier.py
@@ -895,16 +895,16 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
     head_loss = 0
     for i in range(modeling.heads):
       mh0_i = tf.get_default_graph().get_tensor_by_name('bert/pooler/mh0_'+str(i)+'/kernel:0') 
-      mh0_i = tf.l2_normalize(mh0_i)
+      mh0_i = tf.math.l2_normalize(mh0_i)
       mh1_i = tf.get_default_graph().get_tensor_by_name('bert/pooler/mh1_'+str(i)+'/kernel:0')
-      mh1_i = tf.l2_normalize(mh1_i)
+      mh1_i = tf.math.l2_normalize(mh1_i)
       for j in range(i+1, modeling.heads):
         mh0_j = tf.get_default_graph().get_tensor_by_name('bert/pooler/mh0_'+str(j)+'/kernel:0')
-        mh0_j = tf.l2_normalize(mh0_j)
+        mh0_j = tf.math.l2_normalize(mh0_j)
         mh1_j = tf.get_default_graph().get_tensor_by_name('bert/pooler/mh1_'+str(j)+'/kernel:0')
-        mh1_j = tf.l2_normalize(mh1_j)
+        mh1_j = tf.math.l2_normalize(mh1_j)
         tmp_loss = tf.reduce_sum(tf.squared_difference(mh0_i, mh0_j))
-        head_loss += tf.losses.mean_squared_error(mh0_i, mh0_j)/(tf.norm(mh0_i) * tf.norm(mh0_j)) + tf.losses.mean_squared_error(mh1_i, mh1_j)/(tf.norm(mh1_i, mh1_j))
+        head_loss += tf.losses.mean_squared_error(mh0_i, mh0_j) + tf.losses.mean_squared_error(mh1_i, mh1_j)/(tf.norm(mh1_i, mh1_j))
 
   with tf.variable_scope("loss"):
     if is_training:
