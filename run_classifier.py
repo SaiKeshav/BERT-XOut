aa48f4fbe954d56b346f81700c414d91afcaeeb3
SaiKeshav <keshavkolluru@gmail.com>
2019-05-06 07:17:36 +0530

    test

--- run_classifier.py
+++ run_classifier.py
@@ -462,9 +462,10 @@ class QQPProcessor(DataProcessor):
     for (i, line) in enumerate(lines):
       if i == 0:
         continue
+      print(len(line))
       guid = "%s-%s" % (set_type, tokenization.convert_to_unicode(line[0]))
-      text_a = tokenization.convert_to_unicode(line[2])
-      text_b = tokenization.convert_to_unicode(line[3])
+      text_a = tokenization.convert_to_unicode(line[3])
+      text_b = tokenization.convert_to_unicode(line[4])
       if set_type == "test":
         label = "0"
       else:
