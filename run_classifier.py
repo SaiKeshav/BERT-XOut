05c81b7c544f0e1225975469cd9cd6069a33d3d3
SaiKeshav <keshavkolluru@gmail.com>
2019-05-06 10:15:31 +0530

    run_classifier:

--- run_classifier.py
+++ run_classifier.py
@@ -691,7 +691,6 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
   for (i, label) in enumerate(label_list):
     label_map[label] = i
 
-  print(label_map)
   tokens_a = tokenizer.tokenize(example.text_a)
   tokens_b = None
   if example.text_b:
