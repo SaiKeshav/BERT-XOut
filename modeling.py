cc4bed9de58a3d342d645e034783be0e9de4e815
SaiKeshav <keshavkolluru@gmail.com>
2019-05-07 15:14:41 +0530

    modeling:

--- modeling.py
+++ modeling.py
@@ -233,9 +233,9 @@ class BertModel(object):
         if(heads != 0):
           embs = [pool(self.sequence_output, 1, pool_type)]
           for i in range(heads):
-            out = tf.layers.dense(self.sequence_output, middle_dim, kernel_initializer=create_uniform_initializer(middle_dim), name='mh0_'+str(i))
+            out = tf.layers.dense(self.sequence_output, middle_dim, kernel_initializer=create_initializer(config.initializer_range), name='mh0_'+str(i))
             out = self.swish(out)
-            out = tf.layers.dense(out, final_dim, activation=tf.tanh, kernel_initializer=create_uniform_initializer(final_dim), name='mh1_'+str(i))
+            out = tf.layers.dense(out, final_dim, activation=tf.tanh, kernel_initializer=create_initializer(config.initializer_range), name='mh1_'+str(i))
             embs.append(pool(out, 1, pool_type))  
           self.pooled_output = tf.concat(embs, 1)
         elif(pool_type != 0):
